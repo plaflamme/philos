ed521ed75c7aa510bf33ba6b7623b92009246e5b
Philippe Laflamme <philippe.laflamme@gmail.com>
2021-01-17 16:44:49 -0500

    feat: test harness

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 #![no_std]
 #![no_main]
+
+// https://os.phil-opp.com/testing/#custom-test-frameworks
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
 
 mod vga_buffer;
@@ -7,6 +13,10 @@ mod vga_buffer;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("{} {}", "Hello", "world!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
@@ -14,4 +24,12 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
