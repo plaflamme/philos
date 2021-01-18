92d16c168b89cc5d513f9d3e1f2f2ab7c2b8fe50
Philippe Laflamme <philippe.laflamme@gmail.com>
2021-01-17 23:04:32 -0500

    feat: adds keyboard

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -12,6 +12,7 @@ lazy_static! {
                 .set_stack_index(crate::gdt::DOUBLE_FAULT_IST_INDEX);
         }
         idt[InterruptIndex::Timer as usize].set_handler_fn(timer_interrupt_handler);
+        idt[InterruptIndex::Keyboard as usize].set_handler_fn(keyboard_interrupt_handler);
         idt
     };
 }
@@ -34,6 +35,33 @@ extern "x86-interrupt" fn timer_interrupt_handler(_: &mut InterruptStackFrame) {
     unsafe { PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer as u8) };
 }
 
+
+extern "x86-interrupt" fn keyboard_interrupt_handler(_: &mut InterruptStackFrame) {
+    // keyboard scancode port
+    let mut port = x86_64::instructions::port::Port::new(0x60);
+    let scancode: u8 = unsafe { port.read() };
+
+    use pc_keyboard::{DecodedKey, Keyboard, ScancodeSet1};
+
+    lazy_static! {
+        static ref KEYBOARD: spin::Mutex<Keyboard<pc_keyboard::layouts::Us104Key, ScancodeSet1>> = {
+            let kb = Keyboard::new(pc_keyboard::layouts::Us104Key, ScancodeSet1, pc_keyboard::HandleControl::Ignore);
+            spin::Mutex::new(kb)
+        };
+    }
+    let mut kb = KEYBOARD.lock();
+    if let Ok(Some(key_event)) = kb.add_byte(scancode) {
+        if let Some(key) = kb.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => print!("{:?}", key),
+            }
+        }
+    }
+
+    unsafe { PICS.lock().notify_end_of_interrupt(InterruptIndex::Keyboard as u8) };
+}
+
 pub const PIC1_OFFSET: u8 = 32;
 pub const PIC2_OFFSET: u8 = PIC1_OFFSET + 8;
 
@@ -48,4 +76,5 @@ pub fn init_pics() {
 #[repr(u8)]
 pub enum InterruptIndex {
     Timer = PIC1_OFFSET,
+    Keyboard,
 }
