ce2eb091b5e2ad6f8d1e695223462619ec8324b8
Philippe Laflamme <philippe.laflamme@gmail.com>
2021-01-17 20:09:57 -0500

    feat: integration test

---
+++ tests/basic_boot.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+
+// https://os.phil-opp.com/testing/#custom-test-frameworks
+#![feature(custom_test_frameworks)]
+#![test_runner(philos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    philos::test_panic_handler(info)
+}
+
+use philos::println;
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
