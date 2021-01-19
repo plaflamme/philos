10a7c8762c8368915d112dbdf761d7fb3354dd8f
Philippe Laflamme <philippe.laflamme@gmail.com>
2021-01-18 19:06:29 -0500

    feat: page fault handler.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,7 +1,7 @@
-use crate::{print, println};
+use crate::{print, println, hlt};
 use lazy_static::lazy_static;
 use pic8259_simple::ChainedPics;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -11,6 +11,8 @@ lazy_static! {
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(crate::gdt::DOUBLE_FAULT_IST_INDEX);
         }
+        idt.page_fault.set_handler_fn(page_fault_interrupt_handler);
+
         idt[InterruptIndex::Timer as usize].set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard as usize].set_handler_fn(keyboard_interrupt_handler);
         idt
@@ -35,6 +37,16 @@ extern "x86-interrupt" fn timer_interrupt_handler(_: &mut InterruptStackFrame) {
     unsafe { PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer as u8) };
 }
 
+extern "x86-interrupt" fn page_fault_interrupt_handler(sf: &mut InterruptStackFrame, error_code: PageFaultErrorCode) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", sf);
+
+    hlt();
+}
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(_: &mut InterruptStackFrame) {
     // keyboard scancode port
